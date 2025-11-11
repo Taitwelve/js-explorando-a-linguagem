563aba7c40d057d096f75a23d011f5bfaff59e96
Taitwelve <taynascimento15@gmail.com>
2025-11-11 12:41:24 -0300

    feat: implement initial list handling with destinations

---
+++ listas.js
@@ -0,0 +1,18 @@
+console.log(`Trabalhando com listas`);
+const salvador = `Salvador`;
+const saoPaulo = `São Paulo`;
+const rioDeJaneiro = `Rio de Janeiro`;
+console.log(`Destinos possíveis: `);
+console.log(salvador, saoPaulo, rioDeJaneiro);
+// criar uma lista com variáveis separadas não é uma boa prática, o ideal é usar arrays
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+listaDeDestinos.push(`Curitiba`); // adicionando um item na lista
+
+console.log(`Destinos possíveis:`);
+console.log(listaDeDestinos);
