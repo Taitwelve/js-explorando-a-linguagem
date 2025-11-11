b1044b28a54b242c8104a800df8ed80fec542707
Taitwelve <taynascimento15@gmail.com>
2025-11-11 11:49:22 -0300

    fix: boas práticas com variáveis

--- trabalhandoComVariaveis.js
+++ trabalhandoComVariaveis.js
@@ -1,2 +1,9 @@
+//Console.log("Trabalhando com variáveis");
 console.log("Trabalhando com variáveis");
 // JS é case sensitive
+
+idade = 30;
+Idade = 40;
+
+console.log(idade);
+console.log(Idade);
