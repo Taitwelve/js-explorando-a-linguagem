3fad8d5c59e43ee6fe239bf18c030aaf8a9838f9
Taitwelve <taynascimento15@gmail.com>
2025-11-11 11:33:09 -0300

    fix: guardando valores na memória do computador

---
+++ meuPrimeiroPrograma.js
@@ -0,0 +1,8 @@
+console.log("Meu primeiro programa em JavaScript! Trabalhando com variáveis");
+
+const idade = 30;
+console.log("Idade:", idade);
+console.log("Idade:", idade+2);
+
+const idadeSomada = idade + 2;
+console.log("Idade somada:", idadeSomada);
