09f89131fa436f8fae5d8f70a358e8029e3b1e36
Taitwelve <taynascimento15@gmail.com>
2025-11-11 12:06:35 -0300

    exemplos de como converter tipos

---
+++ conversaoDeTipos.js
@@ -0,0 +1,12 @@
+console.log("Trabalhando com conversão de tipos");
+console.log("ano: " + 2020);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+console.log("10" / "2");
+// o Js converteu as strings para números automaticamente, pois a operação de divisão só faz sentido com números
+console.log ("Ricardo" / "2");
+// NaN - Not a Number (não é um número)
+// O Js não conseguiu converter as strings para números, então o resultado é NaN
+console.log("7" / "2");
+console.log(6,5);
+console.log(6.5);
