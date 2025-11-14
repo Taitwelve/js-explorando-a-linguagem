a76af28d48809308e1f0874cd54e3ffe324913e2
Taitwelve <taynascimento15@gmail.com>
2025-11-14 14:38:36 -0300

    feat: update age logic for ticket sales and boarding checks

--- condicionais.js
+++ condicionais.js
@@ -6,19 +6,24 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
-const idadeComprador = 16;
+const idadeComprador = 20;
 const estaAcompanhado = true;
+const passagemComprada = true;
 
 console.log(`Destinos possíveis:`);
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-    console.log(`Comprador maior de idade`);
+if (idadeComprador >= 18 || estaAcompanhado == true) {
+    console.log(`Passagem vendida com sucesso`);
     listaDeDestinos.splice(1,1);
-} else if (estaAcompanhado) {
-        console.log(`Comprador está acompanhado`);
-        listaDeDestinos.splice(1,1);
 } else {
-    console.log(`Comprador menor de idade. Não é possível vender passagem`);
+    console.log(`Comprador menor de idade e sem acompanhante. Não é possível vender a passagem!`);
 }
 console.log(listaDeDestinos);
+
+console.log(`Checagem de embarque:`);
+if (passagemComprada && idadeComprador >= 18) {
+    console.log(`Embarque liberado. Boa viagem!`);
+} else {
+    console.log(`Você não pode embarcar!`);
+}
